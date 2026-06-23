b0451f005bc64630ce333a2260f2bdae2586806e
shriraotanvi <151399496+shriraotanvi@users.noreply.github.com>
2026-06-23 12:46:06 +0530

    Time: 0 ms (100%), Space: 20.4 MB (19.74%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.c
@@ -0,0 +1,44 @@
+void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
+
+    int i, j;
+    int col0 = 1;
+    int n = matrixSize;
+    int m = *matrixColSize;
+
+    for(i=0; i<n; i++){
+        for(j=0; j<m; j++){
+            if(matrix[i][j] == 0){
+                matrix[i][0] = 0;
+
+                if(j!=0){
+                    matrix[0][j]=0;
+                }
+                else{
+                    col0 = 0;
+                }
+            }
+        }
+    }
+
+    for(i=1; i<n; i++){
+        for(j=1; j<m; j++){
+            if((matrix[i][0] == 0) || (matrix[0][j] == 0)){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if(matrix[0][0] == 0){
+        for(j=0; j<m; j++){
+            matrix[0][j] = 0;
+        }
+    }
+
+    if(col0 == 0){
+        for(i=0; i<n; i++){
+            matrix[i][0]=0;
+        }
+    }
+
+    
+}
