5a460132cc88da3efd11152dbe47ee82efa893e8
shriraotanvi <151399496+shriraotanvi@users.noreply.github.com>
2026-08-09 16:52:21 +0530

    Time: 0 ms (100%), Space: 22.3 MB (17.79%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -15,6 +15,6 @@ public:
         if(root == NULL) return 0;
         int left = maxDepth(root->left);
         int right = maxDepth(root->right);
-        return 1 + max(left, right);
+        return 1+max(left, right);
     }
 };
