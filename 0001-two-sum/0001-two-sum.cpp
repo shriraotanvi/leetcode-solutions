b99391b98efcdec8f076ca2bd6ebba7fd938b553
shriraotanvi <151399496+shriraotanvi@users.noreply.github.com>
2026-09-03 20:03:59 +0530

    Time: 2 ms (72.67%), Space: 15 MB (27.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,9 +4,9 @@ public:
         map<int, int> mpp;
         for(int i=0; i<nums.size(); i++){
             int a = nums[i];
-            int more = target - a;
-            if(mpp.find(more) != mpp.end()){
-                return {mpp[more], i};
+            int newTarget = target - a;
+            if(mpp.find(newTarget) != mpp.end()){
+                return {mpp[newTarget], i};
             }
             mpp[a] = i;
         }
